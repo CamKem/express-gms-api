70994007e662f33d64aa27fbf840a7dd964faa95
Cam Kemshal-Bell <cam@iterated.tech>
2024-11-30 22:00:18 +1100

    feat(init): simplify the responsable errors

--- utils/errors.js
+++ utils/errors.js
@@ -1,53 +1,48 @@
-export class BadRequestError extends BaseError {
-    constructor(message = 'The request could not be understood by the server due to malformed syntax.', details) {
-        super(message);
-        this.name = 'BadRequestError';
-        this.statusCode = 400;
-        this.details = details;
-    }
-}
-
-export class UnauthorizedError extends BaseError {
-    constructor(message = 'You are not authorized to access this resource.', details) {
-        super(message);
-        this.name = 'UnauthorizedError';
-        this.statusCode = 401;
-        this.details = details;
-    }
-}
-
-export class ForbiddenError extends BaseError {
-    constructor(message = 'You do not have permission to access this resource.', details) {
-        super(message);
-        this.name = 'ForbiddenError';
-        this.statusCode = 403;
-        this.details = details;
-    }
-}
-
-export class NotFoundError extends BaseError {
-    constructor(message = 'The requested resource was not found.', details) {
-        super(message);
-        this.name = 'NotFoundError';
-        this.statusCode = 404;
-        this.details = details;
-    }
-}
-
-
-export class InternalServerError extends BaseError {
-    constructor(message = 'An internal server error occurred.', details) {
-        super(message);
-        this.name = 'InternalServerError';
-        this.statusCode = 500;
-        this.details = details;
-    }
-}
-
-class BaseError extends Error {
-    constructor(message = 'An error occurred', details) {
+class ResponsableError extends Error {
+    constructor(message = 'An error occurred', statusCode = 500, details) {
         super(message);
         this.name = this.constructor.name;
+        this.statusCode = statusCode;
         this.details = details;
     }
 }
+
+class BadRequestError extends ResponsableError {
+    constructor(message = 'The request could not be understood by the server due to malformed syntax.', details) {
+        super(message, 400, details);
+    }
+}
+
+class UnauthorizedError extends ResponsableError {
+    constructor(message = 'You are not authorized to access this resource.', details) {
+        super(message, 401, details);
+    }
+}
+
+class ForbiddenError extends ResponsableError {
+    constructor(message = 'You do not have permission to access this resource.', details) {
+        super(message, 403, details);
+    }
+}
+
+class NotFoundError extends ResponsableError {
+    constructor(message = 'The requested resource was not found.', details) {
+        super(message, 404, details);
+    }
+}
+
+
+class InternalServerError extends ResponsableError {
+    constructor(message = 'An internal server error occurred.', details) {
+        super(message, 500, details);
+    }
+}
+
+export {
+    BadRequestError,
+    UnauthorizedError,
+    ForbiddenError,
+    NotFoundError,
+    InternalServerError,
+    ResponsableError
+};
