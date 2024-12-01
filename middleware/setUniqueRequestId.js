01d99111b229626adf64df3ca90a031c08383cf5
Cam Kemshal-Bell <cam@iterated.tech>
2024-12-01 18:28:42 +1100

    feat(init): add middleware to set a request ID

---
+++ middleware/setUniqueRequestId.js
@@ -0,0 +1,18 @@
+import { v5 as uuidv5 } from 'uuid';
+import {setValue} from "../utils/setValue.js";
+
+/**
+ * Sets a unique request ID for each request
+ * @param req
+ * @param res
+ * @param next
+ * @type {function(*, *, *): void}
+ * @returns {void}
+ */
+
+const setUniqueRequestId = (req, res, next) => {
+    req.requestId = setValue(req.requestId, uuidv5(Date.now().toString(), uuidv5.URL));
+    next();
+}
+
+export default setUniqueRequestId;
