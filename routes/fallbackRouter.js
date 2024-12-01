b0211805b02855e73d2fe15a588f0da1e79de380
Cam Kemshal-Bell <cam@iterated.tech>
2024-12-01 18:26:13 +1100

    feat(init): make our 404's return a RESOURCE_NOT_FOUND code

--- routes/fallbackRouter.js
+++ routes/fallbackRouter.js
@@ -6,6 +6,7 @@ const fallbackRouter = express.Router();
 fallbackRouter.use((req, res, next) => {
     next(new NotFoundError()
         .withDetails('Oops! The resource you are looking for does not exist, or has been moved. Please refer to the API documentation for more information.')
+        .withCode('RESOURCE_NOT_FOUND')
     );
 });
 
