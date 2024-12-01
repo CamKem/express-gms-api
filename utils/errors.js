9a0d352b7462e3dffd7bc7db45583b865a54a3bd
Cam Kemshal-Bell <cam@iterated.tech>
2024-12-01 13:52:43 +1100

    feat(init): add the human readable name for each custom responsable error

--- utils/errors.js
+++ utils/errors.js
@@ -10,24 +10,56 @@ class ResponsableError extends Error {
 class BadRequestError extends ResponsableError {
     constructor(message = 'The request could not be understood by the server due to malformed syntax.', details) {
         super(message, 400, details);
+        this.name = 'Bad Request';
     }
 }
 
 class UnauthorizedError extends ResponsableError {
     constructor(message = 'You are not authorized to access this resource.', details) {
         super(message, 401, details);
+        this.name = 'Unauthorized';
     }
 }
 
 class ForbiddenError extends ResponsableError {
     constructor(message = 'You do not have permission to access this resource.', details) {
         super(message, 403, details);
+        this.name = 'Forbidden';
     }
 }
 
 class NotFoundError extends ResponsableError {
     constructor(message = 'The requested resource was not found.', details) {
         super(message, 404, details);
+        this.name = 'Not Found';
+    }
+}
+
+class MethodNotAllowedError extends ResponsableError {
+    constructor(message = 'The method is not allowed for the requested URL.', details) {
+        super(message, 405, details);
+        this.name = 'Method Not Allowed';
+    }
+}
+
+class NotAcceptableError extends ResponsableError {
+    constructor(message = 'The server cannot generate a response that the client will accept.', details) {
+        super(message, 406, details);
+        this.name = 'Not Acceptable';
+    }
+}
+
+class ConflictError extends ResponsableError {
+    constructor(message = 'The request could not be completed due to a conflict with the current state of the resource.', details) {
+        super(message, 409, details);
+        this.name = 'Conflict';
+    }
+}
+
+class UnsupportedMediaTypeError extends ResponsableError {
+    constructor(message = 'The server does not support the media type transmitted in the request.', details) {
+        super(message, 415, details);
+        this.name = 'Unsupported Media Type';
     }
 }
 
@@ -35,6 +67,7 @@ class NotFoundError extends ResponsableError {
 class InternalServerError extends ResponsableError {
     constructor(message = 'An internal server error occurred.', details) {
         super(message, 500, details);
+        this.name = 'Internal Server Error';
     }
 }
 
@@ -44,5 +77,9 @@ export {
     ForbiddenError,
     NotFoundError,
     InternalServerError,
-    ResponsableError
+    ResponsableError,
+    NotAcceptableError,
+    ConflictError,
+    MethodNotAllowedError,
+    UnsupportedMediaTypeError
 };
