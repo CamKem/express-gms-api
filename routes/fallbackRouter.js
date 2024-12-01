ff9730a446f567b2ef5bdbdacfc1163b2a5f7fed
Cam Kemshal-Bell <cam@iterated.tech>
2024-12-01 15:58:06 +1100

    feat(init): implement a fallbackRouter to handle all 404 requests using our handler

---
+++ routes/fallbackRouter.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { NotFoundError } from '../utils/errors.js';
+
+const fallbackRouter = express.Router();
+
+fallbackRouter.use((req, res, next) => {
+    next(new NotFoundError()
+        .withDetails('Oops! The resource you are looking for does not exist, or has been moved. Please refer to the API documentation for more information.')
+    );
+});
+
+export default fallbackRouter;
