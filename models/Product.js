ff59f2e9fd4d4dd671491a85289bd4b47b73b525
Cam Kemshal-Bell <cam@iterated.tech>
2024-12-02 15:53:47 +1100

    feat(init): update product schema

--- models/Product.js
+++ models/Product.js
@@ -5,25 +5,40 @@ const ProductSchema = new mongoose.Schema(
         sku: {
             type: String,
             required: true,
-            unique: true, // Ensures uniqueness on the SKU field
+            trim: true,
+            unique: true,
+            validate: {
+                validator: (v) => /^[A-Z]{2}-\d{4}-\d{2}$/.test(v),
+                message: (props) => {
+                    return `${props.value} is not a valid SKU!`
+                        + `A valid SKU should be in the format of XX-1234-56`;
+                }
+            }
         },
         name: {
             type: String,
             required: true,
+            trim: true,
+            validate: {
+                validator: (v) => /^[a-zA-Z ]+$/.test(v),
+                message: (props) => {
+                    `${props.value} is not a valid name! (letters and spaces only)`
+                },
+            },
         },
         price: {
             type: Number,
             required: true,
+            min: [0, 'Price must be a non-negative number'],
         },
         stockOnHand: {
             type: Number,
             required: true,
+            min: [0, 'Stock must be a non-negative number'],
         },
-    },
-    {
-        timestamps: true, // Adds createdAt and updatedAt timestamps
-    }
-);
+    }, {
+        timestamps: true,
+    });
 
 const Product = mongoose.model('Product', ProductSchema);
 
