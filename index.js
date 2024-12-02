5db612e22b037bd43d905cc388514ceef98bf284
Cam Kemshal-Bell <cam@iterated.tech>
2024-12-02 13:22:06 +1100

    feat(init): move routes into the connection callback

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import errorHandler from './middleware/errorHandler.js';
 import {setValue} from "./utils/setValue.js";
 import setUniqueRequestId from "./middleware/setUniqueRequestId.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import logger from "./utils/logger.js";
 
 // Load environment variables
 dotenv.config();
@@ -29,14 +30,6 @@ app.use(cors({
     optionsSuccessStatus: 200
 }));
 
-// Routers
-app.use('/', rootRouter);
-app.use('/api', apiRouter);
-app.use(fallbackRouter);
-
-// Global Error/Exception Handler
-app.use(errorHandler);
-
 // Mongoose Connection Error Handler
 mongoose.connection.on("error", (err) => {
     console.error("DB error: " + err);
@@ -46,11 +39,25 @@ mongoose.connection.on("error", (err) => {
 mongoose.connect(process.env.DB_CONNECTION_STRING)
     .then(() => {
         console.log('Database connected');
-        app.listen(port, () => {
-            console.log(`Server running on ${host}:${port}`);
-        });
+
+        // Routers
+        app.use('/', rootRouter);
+        app.use('/api', apiRouter);
+        app.use(fallbackRouter);
+
+        // Global Error/Exception Handler
+        app.use(errorHandler);
+
+        const isMainFile = process.argv.filter(arg => arg.includes('index.js')).length > 0;
+        if (isMainFile) {
+            app.listen(port, () => {
+                console.log(`Server running on ${host}:${port}`);
+            });
+        }
+
+        console.log('Server setup complete');
     })
     .catch(err => {
-        console.error('Database connection error:', err);
+        logger('error', 'DB Connection', err);
         process.exit(1);
     });
