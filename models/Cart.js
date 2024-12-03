6a7c2a86ffa7a7249ab8962cee1fc9efebc0e730
Cam Kemshal-Bell <cam@iterated.tech>
2024-12-03 22:35:10 +1100

    feat(init): build up Cart model

--- models/Cart.js
+++ models/Cart.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import Product from "./Product.js";
+import { BadRequestError } from '../utils/errors.js';
 
 const ProductSchema = new mongoose.Schema({
     sku: {
@@ -7,13 +8,13 @@ const ProductSchema = new mongoose.Schema({
         required: true,
         trim: true,
         match: [/^[A-Z]{2}-\d{4}-\d{2}$/, '{PATH} is not a valid SKU! A valid SKU should be in the format of XX-1234-56'],
-        validate: {
-            validator: async function (sku) {
-                const product = await Product.findOne({ sku });
-                return !!product;
-            },
-            message: props => `SKU '${props.value}' does not exist in the Product collection.`,
-        },
+        // validate: {
+        //     validator: async function (sku) {
+        //         const product = await Product.findOne({ sku });
+        //         return !!product;
+        //     },
+        //     message: props => `SKU '${props.value}' does not exist in the Product collection.`,
+        // },
     },
     quantity: {
         type: Number,
@@ -25,12 +26,15 @@ const ProductSchema = new mongoose.Schema({
 const CartSchema = new mongoose.Schema({
     customerNo: {
         type: Number,
-        required: true,
+        required: [true, 'Customer number is required'],
+        minlength: [8, 'Customer number must be an 8-digit number'],
+        maxlength: [8, 'Customer number must be an 8-digit number'],
+        match: [/^\d{8}$/, 'Customer number must be an 8-digit number'],
         unique: true,
     },
     products: {
         type: [ProductSchema],
-        validate: [arrayLimit, '{PATH} exceeds the limit of 100'],
+        validate: [arrayLimit, 'A cart cannot have more than 100 products.'],
         default: [],
     },
     createdAt: {
@@ -44,11 +48,38 @@ const CartSchema = new mongoose.Schema({
     },
 }, {
     timestamps: { createdAt: true, updatedAt: true },
+    versionKey: false,
 });
 
 function arrayLimit(val) {
     return val.length <= 100;
 }
+
+CartSchema.pre('save', async function (next) {
+    try {
+        const skus = this.products.map(product => product.sku);
+        const existingProducts = await Product
+            .find({ sku: { $in: skus } })
+            .select('sku');
+
+        const existingSkus = existingProducts.map(product => product.sku);
+        const invalidSkus = skus.filter(sku => !existingSkus.includes(sku));
+
+        if (invalidSkus.length > 0) {
+            return next(
+                new BadRequestError('Invalid SKU(s) found in the cart. Please remove them and try again.')
+                    .withDetails(`SKUs: ${invalidSkus.join(', ')}`)
+                    .withCode('CART_SKU_INVALID')
+                    .withDocsUrl(`${process.env.DOCUMENTATION_URL}/cart#invalid-skus`)
+            );
+
+        }
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 
 const Cart = mongoose.model('Cart', CartSchema);
 
