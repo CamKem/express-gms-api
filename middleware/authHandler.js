11a4fa4cea8abe1d263bab6e744ba099ee44ec19
Cam Kemshal-Bell <cam@iterated.tech>
2024-12-04 13:10:46 +1100

    feat(auth): add more informative error responses

--- middleware/authHandler.js
+++ middleware/authHandler.js
@@ -15,19 +15,33 @@ import {UnauthorizedError} from '../utils/errors.js';
  * @param {Function} next - Express next middleware function
  */
 const auth = async (req, res, next) => {
-    const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
+    const token = req.cookies?.token || req.headers.authorization?.split(' ');
 
     if (!token) {
-        throw new UnauthorizedError('Authentication token is missing.')
-            .withCode('TOKEN_MISSING')
-            .withDetails('Please log in to access this resource.')
+        throw new UnauthorizedError('Authorization header is missing.')
+            .withCode('AUTHORIZATION_HEADER_MISSING')
+            .withDetails('Please add in this format: Authorization: Bearer <token>')
             .withDocsUrl('/api/v1/docs#login-an-employee');
     }
 
-    await jwt.verify(token, process.env.JWT_SECRET, async (error, decoded) => {
+    if (token && token[0].toLowerCase() !== 'bearer') {
+        throw new UnauthorizedError('Authentication: Bearer <token> format is required.')
+            .withCode('AUTHORIZATION_HEADER_INVALID')
+            .withDetails('Please ensure that you have the Bearer keyword before the token.')
+            .withDocsUrl('/api/v1/docs#login-an-employee');
+    }
+
+    if (token && !token[1]) {
+        throw new UnauthorizedError('Authentication: bearer token is missing.')
+            .withCode('AUTHORIZATION_HEADER_INVALID')
+            .withDetails('Please ensure that you have a token after the Bearer keyword.')
+            .withDocsUrl('/api/v1/docs#login-an-employee');
+    }
+
+    await jwt.verify(token[1], process.env.JWT_SECRET, async (error, decoded) => {
         if (error) {
-            throw new UnauthorizedError('Invalid authentication token.')
-                .withDetails('Please log in again to access this resource.')
+            throw new UnauthorizedError('Authentication failure: invalid token.')
+                .withDetails(error)
                 .withDocsUrl('/api/v1/docs#login-an-employee')
                 .withCode('INVALID_TOKEN');
         }
@@ -41,7 +55,7 @@ const auth = async (req, res, next) => {
             .catch(error => {
                 if (error) {
                     throw new UnauthorizedError('Employee associated with this token does not exist.')
-                        .withCode('USER_NOT_FOUND')
+                        .withCode('EMPLOYEE_NOT_FOUND')
                         .withDetails('Please log in again.')
                         .withDocsUrl('/api/v1/docs#login-an-employee');
                 }
