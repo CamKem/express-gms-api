ac69ffcfeda797cc653ec888dbf05a546c2aeb8e
Cam Kemshal-Bell <cam@iterated.tech>
2024-12-05 19:52:03 +1100

    feat(api): finalise the dynamic api router

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,83 +1,46 @@
 import express from 'express';
 import fs from 'fs';
-import {NotFoundError, UnprocessableEntityError} from "../utils/errors.js";
 import * as path from "node:path";
-import * as fsPromises from "node:fs/promises";
+import validateApiVersion from "../middleware/apiVersionValidator.js";
 import jsonParser from "../middleware/jsonParser.js";
-import {setValue} from "../utils/setValue.js";
 import contentNegotiator from "../middleware/contentNegotiator.js";
 
 const apiRouter = express.Router();
 
-const routeMap = {};
+apiRouter.use(validateApiVersion);
+apiRouter.use(contentNegotiator);
+apiRouter.use(jsonParser);
 
-const controllersPath = path.resolve('./controllers');
-const apiVersionDirs = (await fsPromises.readdir(controllersPath)).filter((dir) => dir.startsWith('api_v'));
+const registerControllers = () => {
+    const controllersPath = path.resolve('./controllers/api');
+    const apiVersionDirs = fs.readdirSync(controllersPath).filter(dir => dir.startsWith('v'));
 
-apiVersionDirs.forEach((apiVersionDir) => {
-    const version = apiVersionDir.replace('api_', '');
-    const versionPath = path.resolve(`./controllers/${apiVersionDir}`);
-    const resourceFiles = fs.readdirSync(versionPath).filter((file) => file.endsWith('.js'));
+    apiVersionDirs.forEach(apiVersionDir => {
+        const versionPath = path.join(controllersPath, apiVersionDir);
+        const resourceFiles = fs.readdirSync(versionPath).filter(file => file.endsWith('.js'));
 
-    routeMap[version] = [];
+        resourceFiles.forEach(file => {
+            const controllerPath = path.join(versionPath, file);
+            const resourceName = path.basename(file, '.js');
 
-    resourceFiles.forEach((file) => {
-        const resource = path.basename(file, '.js');
-        routeMap[version].push(resource);
-    });
-});
+            const controllerModule = import(controllerPath);
+            controllerModule.then(module => {
+                module = module.default;
 
-const currentVersionNumber = Math.max(...Object.keys(routeMap)
-    .map((version) => parseInt(version.replace('v', ''), 10)));
-const currentVersion = setValue(`v${currentVersionNumber}`, 'v1');
+                if (typeof module !== 'function' || !(module instanceof express.Router)) {
+                    console.error(`Invalid controller configuration in ${controllerPath}`);
+                    return;
+                }
 
-apiRouter.use(async (request, response, next) => {
-    const version = setValue(request.url.split('/')[1], null);
-    const versionNumber = setValue(parseInt(version.replace('v', ''), 10), 0);
-    const endpointDocsUrl = `/docs/api/${currentVersion}/`;
-
-    if (!version || !/^v\d+$/.test(version)) {
-        throw new NotFoundError('No API version specified')
-            .withDetails('Please specify an an API version and check the documentation')
-            .withCode('VERSION_NOT_SPECIFIED')
-            .withDocsUrl(endpointDocsUrl);
-    }
-
-    const folderPath = path.resolve('controllers', `api_v${versionNumber}`);
-    if (!(versionNumber <= currentVersionNumber) || !(versionNumber > 0) || !fs.existsSync(folderPath)) {
-        throw new UnprocessableEntityError('Invalid API version requested')
-            .withDetails(`The current API version is ${currentVersion}`)
-            .withCode('INVALID_API_VERSION')
-            .withDocsUrl(endpointDocsUrl);
-    }
-
-    next();
-});
-
-for (const [version, resources] of Object.entries(routeMap)) {
-    resources.forEach((resource) => {
-        const routePath = `/${version}/${resource}`;
-        const resourceRouter = express.Router();
-
-        resourceRouter.use(contentNegotiator);
-        resourceRouter.use(jsonParser);
-
-        resourceRouter.use(async (req, res, next) => {
-            const controllerPath = path.resolve('controllers', `api_${version}`, `${resource}.js`);
-
-            const controllerModule = await import(controllerPath);
-            const controller = controllerModule.default;
-
-            if (typeof controller !== 'function' || !(controller instanceof express.Router)) {
-                const error = new Error('Invalid controller configuration');
-                return next(error);
-            }
-
-            return controller(req, res, next);
+                apiRouter.use(`/${apiVersionDir}/${resourceName}`, module);
+            })
+                .catch(err => {
+                    throw new Error(`Error loading controller: ${controllerPath}, ${err}`);
+                });
         });
-
-        apiRouter.use(routePath, resourceRouter);
     });
-}
+};
 
-export { apiRouter, routeMap };
+registerControllers();
+
+export default apiRouter;
