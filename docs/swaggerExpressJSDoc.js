5ceddaac9029bc9a3e84865dec99ac123515ca05
Cam Kemshal-Bell <cam@iterated.tech>
2024-12-07 19:17:37 +1100

    feat(swagger): add swaggerExpressJSDoc.js

---
+++ docs/swaggerExpressJSDoc.js
@@ -0,0 +1,56 @@
+// swaggerExpressJSDoc.js
+
+import expressJsdocSwagger from 'express-jsdoc-swagger';
+
+// Swagger definition
+const options = {
+    info: {
+        version: '1.0.0',
+        title: 'GMS API Documentation',
+        description: 'API Documentation for the GMS API',
+    },
+    servers: [
+        {
+            url: 'http://localhost:3000/api/{version}',
+            description: 'Development server',
+            variables: {
+                version: {
+                    enum: [
+                        'v1',
+                        'v2'
+                    ],
+                    default: 'v2',
+                    description: 'API version',
+                }
+            }
+        },
+    ],
+    security: {
+        BearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: "JWT"
+        }
+    },
+    baseDir: process.cwd(), // Your project's root directory
+    filesPattern: [
+        './controllers/api/**/*.js', // Path to your route files
+        './models/**/*.js', // Path to your model files
+        './middleware/**/*.js', // Path to your middleware files
+        './utils/**/*.js', // Path to your utility files
+    ],
+    swaggerUIPath: '/api-docs', // Endpoint for Swagger UI rendering
+    exposeSwaggerUI: true,
+    exposeApiDocs: false,
+    apiDocsPath: '/{version}/api-docs', // we need to allow for versioning, to assist with dynamic versioning to ensure the correct version is displayed
+    // You can add more options as needed
+    notRequiredAsNullable: false,
+};
+
+// Initialize Swagger
+const setupExpressJSDocSwagger = (app) => {
+    expressJsdocSwagger(app)(options);
+    console.log('express-jsdoc-swagger is available at /api-docs');
+};
+
+export default setupExpressJSDocSwagger;
